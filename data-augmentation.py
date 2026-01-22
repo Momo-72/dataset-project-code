9b2e428a171e5cf651c221554892934a478be0a3
Mo <moaeasweden@gmail.com>
2026-01-22 13:20:04 +0800

    Updated data-augmentation.py according to meeting

--- data-augmentation.py
+++ data-augmentation.py
@@ -6,18 +6,18 @@ def stitch_image(input_path, output_path, copies=10, mode="default"):
     img = Image.open(input_path)
     width, height = img.size
 
-    if (mode == "default"):
+    if mode == "default":
         new_img = Image.new("RGB", (width * copies, height * copies))
         for i in range(copies):
             for j in range(copies):
                 new_img.paste(img, (i * width, j * height))
 
-    elif (mode == "horizontal"):
+    elif mode == "horizontal":
         new_img = Image.new("RGB", (width * copies, height))
         for i in range(copies):
             new_img.paste(img, (i * width, 0))
 
-    elif (mode == "vetical"):
+    elif mode == "vertical":
         new_img = Image.new("RGB", (width, height * copies))
         for i in range(copies):
             new_img.paste(img, (0, i * height))
@@ -25,35 +25,39 @@ def stitch_image(input_path, output_path, copies=10, mode="default"):
     else:
         raise ValueError("Invalid mode provided.")
 
-    
     new_img.save(output_path)
+    return new_img  # return stitched image for further use
 
-def random_crop(input_path, output_path, crop_width, crop_height):
-    img = Image.open(input_path)
+
+def random_crop(img, crop_width, crop_height):
     width, height = img.size
+
+    if crop_width > width or crop_height > height:
+        raise ValueError("Crop size larger than image dimensions.")
 
     left = random.randint(0, width - crop_width)
     top = random.randint(0, height - crop_height)
     right = left + crop_width
     bottom = top + crop_height
-    
-    new_img = img.crop((left, top, right, bottom))
 
-    new_img.save(output_path)
+    return img.crop((left, top, right, bottom))
+
 
 if __name__ == "__main__":
-    folder = "Image_Folder" # change this to working folder
-    output_folder = "Output" # Change this to where want to save
+    folder = "Image_Folder"  # change this to working folder
+    output_folder = "Output"  # change this to where you want to save
     os.makedirs(output_folder, exist_ok=True)
 
     for filename in os.listdir(folder):
-        if filename.lower.endswith((".jpg", ".jpeg", ".png")):
+        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
             input_path = os.path.join(folder, filename)
 
+            # Step 1: Stitch image
             stitched_path = os.path.join(output_folder, f"stitched_{filename}")
-            stitch_image(input_path, stitched_path, copies=10, mode="default")
+            stitched_img = stitch_image(input_path, stitched_path, copies=10, mode="default")
 
-            random_path = os.path.join(output_folder, f"random_crop_{filename}")
-            random_crop(input_path, random_path, crop_width=100, crop_height=100)
-
-
+            # Step 2: Crop stitched image 10 times
+            for i in range(10):
+                random_crop_img = random_crop(stitched_img, crop_width=100, crop_height=100)
+                random_path = os.path.join(output_folder, f"random_crop_{i+1}_{filename}")
+                random_crop_img.save(random_path)
